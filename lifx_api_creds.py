6ffd065fdfdd75498fa6fa13901df2034716a88e
Noah Norman <doctorhandshake@gmail.com>
2015-10-21 13:49:55 -0400

    singleton mod for api headers

---
+++ lifx_api_creds.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 21 13:08:21 2015
+
+@author: handsheik
+"""
+import json
+
+def load_data():  
+    with open('data.json') as data_file:    
+        return json.load(data_file)
+        
+def generate_token():
+    return load_data()['token']
+    
+def generate_headers():
+    global token    
+    return {"Authorization": "Bearer %s" % token}
+    
+token = generate_token()
+print 'token generated: ', token
+headers = generate_headers()
+print 'headers generated: ', headers
