2dc8c011162277d1f422115b4fbffab0c21ed347
Noah Norman <doctorhandshake@gmail.com>
2015-11-06 12:39:03 -0500

    broke out lightstate

--- lifx_circ_trig.py
+++ lifx_circ_trig.py
@@ -23,6 +23,7 @@ import time
 
 import ephem
 import creds
+from lightstate import LightState
 #import tornado.websocket
 #import tornado.httpserver
 #import tornado.ioloop
@@ -30,34 +31,7 @@ import creds
 lights_on = True
 LOG_FILENAME = 'logs/lifx_circ_trig.log'
 
-class LightState():
-    def __init__(self, name, bright, start,
-                 hue=None, sat=None, kelvin=None):
-        self.name = name
-        self.bright = bright
-        self.start = start
-        self.hue = hue
-        self.sat = sat
-        self.kelvin = kelvin
-        if hue is None:
-            self.type = "white"
-        else:
-            self.type = "color"
 
-    def __repr__(self):
-        rep = "///LS NAME: {}, ".format(self.name)
-        rep += "bright: {}, ".format(self.bright)
-        rep += "start: {}, ".format(self.start)
-        rep += "hue: {}, ".format(self.hue)
-        rep += "sat: {}, ".format(self.sat)
-        rep += "kelvin: {} ".format(self.kelvin)
-        return  rep
-
-    def is_color(self):
-        return self.type is "color"
-
-    def is_white(self):
-        return self.type is "white"
 
 #class IndexHandler(tornado.web.RequestHandler):
 #    """HTTP request handler to serve HTML for switch server"""
